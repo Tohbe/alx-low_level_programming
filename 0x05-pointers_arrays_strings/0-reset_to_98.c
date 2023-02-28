2d1060cdf4d08a35b600b1aea57bdba1142f5795
“Tohbe” <tobbyflashy@gmail.com>
2023-02-28 09:14:05 -0800

    ./0-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to98 - update the value of n to 98
  * @n: input
  * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }	
