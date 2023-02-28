368420b3fbfc5e993dfc41cae506e6ebb1d67d65
“Tohbe” <tobbyflashy@gmail.com>
2023-02-28 13:05:33 -0800

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,16 +3,14 @@
 /**
  *  _strlen - Returns the length of a string.
  *  @str: The string to get the length of 
- *  Retuen: The length of @str.
+ *  Retuen: The length of @str
  */
 size_t _strlen(const char *str)
-
 {
-	size_t length =0;
+	size_t length = 0;
 
 	while (*str++)
 	length++;
 	return (length);
-
 }
 
