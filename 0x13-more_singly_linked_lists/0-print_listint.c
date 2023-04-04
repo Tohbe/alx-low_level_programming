b8dbf07110c3dba20463274e4d1e63437e40cdce
“Tohbe” <tobbyflashy@gmail.com>
2023-04-04 16:33:16 -0700

    correction on 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_cnt = 0;
+    size_t node_cnt = 0;
 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		node_cnt++;
-	}
-	return (node_cnt);
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        node_cnt++;
+    }
+
+    return (node_cnt);
 }
+
